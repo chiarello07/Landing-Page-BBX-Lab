b895c3f77a72c5c7245f4218a3f90dd07b484f60
Skip <skip@usecurling.com>
2026-07-20 20:30:44 -0300

    Increase logo size by 3x for branding

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import logoUrl from '@/assets/blackboxtagline202607201933100007-a6855.png'
 export function Logo() {
   return (
     <div className="flex items-center">
-      <img src={logoUrl} alt="Black Box Lab Logo" className="h-8 md:h-12 w-auto object-contain" />
+      <img src={logoUrl} alt="Black Box Lab Logo" className="h-24 md:h-36 w-auto object-contain" />
     </div>
   )
 }
